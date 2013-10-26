b9101d9f59dc43fccb152ebfdb4945704fdfe627
StanleyFung <stanleyfung94@hotmail.com>
2013-10-26 19:13:07 -0400

    Added Main Menu

--- gen/com/gekkotta/productions/knit/R.java
+++ gen/com/gekkotta/productions/knit/R.java
@@ -22,22 +22,29 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int listitem_bg=0x7f020001;
+        public static final int listitem_bg_hover=0x7f020002;
+        public static final int listitem_selector=0x7f020003;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f080006;
-        public static final int action_settings=0x7f08000d;
-        public static final int autoCompleteTextView1=0x7f08000b;
-        public static final int imageButton1=0x7f08000a;
-        public static final int list=0x7f080008;
-        public static final int listView1=0x7f08000c;
-        public static final int newProject=0x7f080007;
-        public static final int progressBar1=0x7f080001;
-        public static final int rowdown=0x7f080004;
-        public static final int rowout=0x7f080002;
-        public static final int rowup=0x7f080005;
-        public static final int textOut=0x7f080003;
-        public static final int textView1=0x7f080009;
-        public static final int textout=0x7f080000;
+        public static final int RelativeLayout1=0x7f08000a;
+        public static final int action_settings=0x7f080011;
+        public static final int autoCompleteTextView1=0x7f08000f;
+        public static final int b_credits=0x7f080001;
+        public static final int b_guess=0x7f080002;
+        public static final int b_projlist=0x7f080000;
+        public static final int b_search=0x7f080003;
+        public static final int imageButton1=0x7f08000e;
+        public static final int list=0x7f08000c;
+        public static final int listView1=0x7f080010;
+        public static final int newProject=0x7f08000b;
+        public static final int progressBar1=0x7f080005;
+        public static final int rowdown=0x7f080008;
+        public static final int rowout=0x7f080006;
+        public static final int rowup=0x7f080009;
+        public static final int textOut=0x7f080007;
+        public static final int textView1=0x7f08000d;
+        public static final int textout=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/gekkotta/productions/knit/ProjectList.java
+++ src/com/gekkotta/productions/knit/ProjectList.java
@@ -1,25 +1,35 @@
 package com.gekkotta.productions.knit;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
-import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 public class ProjectList extends Activity{
 	
+	ListView list;
+	ListAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	
-		//setContentView(R.layout.projectlist);
-
+		setContentView(R.layout.projlist);
+		list= (ListView)findViewById(R.id.list);
+		adapter = new ProjectListAdapter(this);
+		list.setAdapter(adapter);
 		
+		list.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	
 	}
 }
