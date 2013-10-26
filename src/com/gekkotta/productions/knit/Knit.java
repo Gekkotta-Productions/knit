950457f239831aa47f80d473ba9a204c709b92a5
AndrewYiuGit <andrewyiu.git@gmail.com>
2013-10-26 19:42:01 -0400

    Fixed progressbar. Limited prev and next to within range of instruction.

--- src/com/gekkotta/productions/knit/Knit.java
+++ src/com/gekkotta/productions/knit/Knit.java
@@ -7,63 +7,66 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+//Displays knitting instructions
 public class Knit extends Activity{
 	
 	int row;
-	int progress;
+	float progress;
 	int totalRows;
 	Button nextRow;
 	Button prevRow;
 	TextView output;
 	TextView rowOutput;
 	ProgressBar pbar;
+	String loc = "Row ";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.knit);
 		//RETRIEVE THESE FROM PHONE DATABASE
-		row = 10;
+		row = 0;
 		totalRows=20; 
-		
-		progress = (row/totalRows) * 100;
+		progress = (row*100)/totalRows;
 		
 		nextRow = (Button) findViewById(R.id.rowup);
 		prevRow = (Button) findViewById(R.id.rowdown);
 		output = (TextView) findViewById(R.id.textout);
 		rowOutput = (TextView) findViewById(R.id.rowout);
 		pbar = (ProgressBar) findViewById(R.id.progressBar1);
-		
-		
-		pbar.setProgress(progress);
+		pbar.setMax(100);
+		pbar.setProgress((int) progress);
+		rowOutput.setText(loc + row);
 		
 		nextRow.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				row++;
-				//Add a call here to save the row progress
-				
-				String loc = "Row" + row;
-				String rowInfo="K1,P1,K2,P3";
-				//TO DO: Set rowInfo to next set of instructions
-				output.setText(rowInfo);
-				rowOutput.setText(loc);
-				
-				progress = (row/totalRows) * 100;
-				pbar.setProgress(progress);
+				if(row < totalRows){
+					row++;
+					//Add a call here to save the row progress
+					
+					String rowInfo="K1,P1,K2,P3";
+					//TO DO: Set rowInfo to next set of instructions
+					output.setText(rowInfo);
+					rowOutput.setText(loc + row);
+					
+					progress = (row*100)/totalRows;
+					pbar.setProgress((int) progress);
+				}
 			}
 		});
 		prevRow.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				row--;
-				//Add a call here to save the row progress
-				
-				String loc = "Row" + row;
-				String rowInfo = "K1,P3";
-				//TO DO: Set rowInfo to next set of instructions
-				output.setText(rowInfo);
-				rowOutput.setText(loc);
-				
-				progress = (row/totalRows) * 100;
-				pbar.setProgress(progress);;
+				if(row > 0){
+					row--;
+					//Add a call here to save the row progress
+	
+					String rowInfo = "K1,P3";
+					//TO DO: Set rowInfo to next set of instructions
+					output.setText(rowInfo);	
+					rowOutput.setText(loc + row);
+					
+					progress = (row*100)/totalRows;
+					pbar.setProgress((int) progress);
+				}
 			}
 		});
 	}
--- src/com/gekkotta/productions/knit/MainActivity.java
+++ src/com/gekkotta/productions/knit/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+//Typical menu
 public class MainActivity extends Activity {
 
 	@Override
